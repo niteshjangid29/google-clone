0b5d0aa2a4202a642f0a622a7e10602889cd7539
niteshjangid29 <niteshjangidofficial@gmail.com>
2022-06-21 17:43:54 +0530

    create footer component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Image from 'next/image';
 import { SearchIcon, MicrophoneIcon} from '@heroicons/react/solid';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
@@ -30,6 +31,7 @@ export default function Home() {
       </form>
 
       {/* Footer */}
+      <Footer />
     </div>
   )
 }
